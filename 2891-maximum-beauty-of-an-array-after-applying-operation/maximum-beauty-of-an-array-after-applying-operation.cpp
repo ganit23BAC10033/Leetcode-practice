272e581bbae9eeddbba0e93d9768cccf6e16b6f2
Ganit Yadav <ganityadav07@gmail.com>
2024-12-11 15:06:08 +0530

    Time: 51 ms (60.73%) | Memory: 104.9 MB (94.06%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -5,13 +5,13 @@ public:
         sort(nums.begin(),nums.end());
         int ans=0;
         int l=0;
-        int r=0;
-        while(r<n){
-            while(nums[r]-nums[l]>2*k){
+        int h=0;
+        while(h<n){
+            while(nums[h]-nums[l]>2*k){
                 l++;
             }
-            ans=max(ans,r-l+1);
-            r++;
+            ans=max(ans,h-l+1);
+            h++;
         }
         return ans;
         
