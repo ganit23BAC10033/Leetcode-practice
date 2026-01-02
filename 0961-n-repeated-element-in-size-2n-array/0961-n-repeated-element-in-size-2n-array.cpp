e3f3c292f5b96c79cfbd43416403c926efde1e98
Ganit Yadav <ganityadav07@gmail.com>
2026-01-02 22:13:30 +0530

    Time: 0 ms (100%), Space: 28.6 MB (42.84%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
         int n=nums.size();
-         
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
-                return nums[i];
-            }
+        unordered_set<int>st;
+        for(int i=0;i<n;i++){
+            if(st.count(nums[i])) return nums[i];
+            st.insert(nums[i]);
         }
         return -1;
     }
