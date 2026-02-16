260576f014d9e07c5f2c230ba40e9a9900c393db
Ganit Yadav <ganityadav07@gmail.com>
2026-02-16 17:10:46 +0530

    Time: 0 ms (100%), Space: 8.1 MB (73.18%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,28 +1,12 @@
 class Solution {
-public:
-    int binarytodecimal(string &s){
-         
-        int n=s.size();
-        int ans=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='1'){
-                ans+=(1<<(n-i-1));
-            }
-        }
-        return ans;
-    }
-    string decimaltobinary(int n){
-        string ans="";
-        for(int i=31;i>=0;i--){
-            if(n&(1<<i)) ans+='1';
-            else ans+='0';
-        }
-        return ans;
-    }
+public: 
     int reverseBits(int n) {
-        string bin=decimaltobinary(n);
-        reverse(bin.begin(),bin.end());
-        int ans=binarytodecimal(bin);
+        int ans=0;
+        for(int i=0;i<32;i++){
+            ans=ans<<1;
+            ans=ans|(1&n);
+            n=n>>1;
+        }
         return ans;
     }
 };
